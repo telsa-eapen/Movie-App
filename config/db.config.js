4fe5c01cf951f9d99326d70f1df595e8b101e739
telsa-eapen <telsaanna15@gmail.com>
2021-06-26 21:20:48 +0530

    checkpoint5

--- config/db.config.js
+++ config/db.config.js
@@ -2,3 +2,6 @@ var mongoClient = require('mongodb').MongoClient;
 
 // Connect to the db
 var dbUrl = "mongodb://localhost:27017";
+module.exports = {
+    url: "mongodb://localhost:27017/moviesdb"
+  };
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http=require('http');
+/*var http=require('http');
 var path=require('path');
 var fs=require('fs');
 var express=require('express');
@@ -13,4 +13,18 @@ http.createServer(function (request, response) {
     if(url=='/genres')
     response.write("All Genres Data in JSON format from Mongo DB");
     response.end();
-  }).listen(port);
+  }).listen(port);*/
+  const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+    
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
